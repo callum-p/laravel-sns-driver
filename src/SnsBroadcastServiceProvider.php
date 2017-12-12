42b00df0d66e40deae50eb71a46a24782186e6b9
Callum Pember <callum@callumpember.com>
2017-12-12 17:07:01 +1000

    test

--- src/SnsBroadcastServiceProvider.php
+++ src/SnsBroadcastServiceProvider.php
@@ -8,15 +8,18 @@ class SnsBroadcastServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		$this->app->make('Illuminate\Broadcasting\BroadcastManager')->extend('sns', function ($app, $config) {
-
-			return new SnsBroadcaster(SnsClient::factory(array(
+			$sns_config = array(
 				'credentials' => array(
 					'key'    => $config['aws_key'],
 					'secret' => $config['aws_secret'],
 				),
 				'version' => 'latest',
-				'region' => $config['aws_region']
-			)));
+				'region' => $config['aws_region'],
+			);
+			if (isset($config['sns_endpoint'])) {
+				$sns_config['endpoint'] = $config['sns_endpoint'];
+			}
+			return new SnsBroadcaster(SnsClient::factory($sns_config));
 		});
 	}
 
